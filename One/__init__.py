aceeb8565b24ba2c98916ef8716f9a8907b2d71b
zhangyin <zhangyin@newlink.com>
2021-12-09 15:40:12 +0800

    one

---
+++ One/__init__.py
@@ -0,0 +1,8 @@
+"""
+@File    :   __init__.py.py    
+@Contact :   zhangyin@newlink.com
+@Modify Time      @Author    @Version
+------------      -------    --------    
+2021/11/27 2:27 下午   张胤        1.0         
+@Desciption：
+"""
---
+++ One/maoli.py
@@ -0,0 +1,21 @@
+"""
+@File    :   maoli.py    
+@Contact :   zhangyin@newlink.com
+@Modify Time      @Author    @Version
+------------      -------    --------    
+2021/12/7 8:31 下午   张胤        1.0         
+@Desciption：
+"""
+
+
+def add():
+    print((9.9 - 9.9 * 0.1) * 50 / (30 + 40 + 50))
+
+
+def jian():
+    print(2.2275 + 2.9699999999999998 + 3.7125 + 9.9 * 0.1)
+
+
+if __name__ == "__main__":
+    add()
+    jian()
