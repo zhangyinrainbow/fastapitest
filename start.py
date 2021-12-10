36d80b999b58c21c0775b14953ec140100c572c9
zhangyin <zhangyin@newlink.com>
2021-12-10 20:29:38 +0800

    update .gitignore3

---
+++ start.py
@@ -0,0 +1,8 @@
+"""
+@File    :   start.py.py    
+@Contact :   zhangyin@newlink.com
+@Modify Time      @Author    @Version
+------------      -------    --------    
+2021/12/10 8:29 下午   张胤        1.0         
+@Desciption：
+"""
