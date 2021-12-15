4ecae667dd600a11c699957bb2fbc921253dd76c
zhangyin <zhangyin@newlink.com>
2021-12-15 11:53:50 +0800

    gitignore

--- start.py
+++ start.py
@@ -1,8 +1,25 @@
-"""
-@File    :   start.py.py    
-@Contact :   zhangyin@newlink.com
-@Modify Time      @Author    @Version
-------------      -------    --------    
-2021/12/10 8:29 下午   张胤        1.0         
-@Desciption：
-"""
+import uvicorn
+from fastapi import FastAPI, HTTPException, Query
+
+app = FastAPI()
+
+people_id_str = {"1": "2"}
+people_id_int = [1, 2, 3, 4, 5]
+
+
+@app.post("/fastapi/{people_idone}")
+async def postdate(
+        people_idone: int,
+        people_id_three: str = Query("1")
+):
+    if people_idone not in people_id_int:
+        raise HTTPException(status_code=400, detail="people_idone不在范围里",headers={"nice":"what"})
+        # 如果people_idone不在列表people_id_int里，那就报错400，并新增一个headers
+    if people_id_three not in people_id_str:
+        raise HTTPException(status_code=401, detail="people_id_three不在范围里")
+        # 如果people_id_three不在people_id_str里，那就报错401
+    return {"people_id": people_idone, "people_id_three": people_id_str[people_id_three]}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app='fastapiOne:app', host='127.0.0.1', port=8100, reload=True, debug=True)
