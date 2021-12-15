7fda584bfc2cd42203353bfaebe973c472546af1
zhangyin <zhangyin@newlink.com>
2021-12-15 13:58:58 +0800

    ignore4

--- start.py
+++ start.py
@@ -22,4 +22,4 @@ async def postdate(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app='start:app', host='127.0.0.1', port=8100, reload=True, debug=True)
+    uvicorn.run(app='start:app', host='0.0.0.0', port=8100, reload=True, debug=True)
